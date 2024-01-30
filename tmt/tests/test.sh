8b0fcdec37af9cb12568dc8300a31c53f6d6ab19
Wei Shi <wshi@redhat.com>
2024-01-30 19:52:14 +0800

    test: updates for a stable branch for testing farm
    Signed-off-by: Wei Shi <wshi@redhat.com>

--- tmt/tests/test.sh
+++ tmt/tests/test.sh
@@ -1,10 +1,17 @@
-#!/bin/sh
+#!/bin/bash
 
 cd ../../
+ARCH=$(uname -m)
+export ARCH
+COMPONENT_NAME=$(echo -n "${SNAPSHOT}" | base64 -d | jq -r .components[0].name)
+CONTAINER_IMAGE=$(echo -n "${SNAPSHOT}" | base64 -d | jq -r .components[0].containerImage)
+echo "${SNAPSHOT}"
+echo "${COMPONENT_NAME}"
+echo "${CONTAINER_IMAGE}"
 
 if [ "$TEST_CASE" = "os-replace" ]; then
-  ./os-replace.sh
+	./os-replace.sh
 else
-  echo "Error: Test case $TEST_CASE not found!"
-  exit 1
+	echo "Error: Test case $TEST_CASE not found!"
+	exit 1
 fi
