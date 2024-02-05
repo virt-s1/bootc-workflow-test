1e9e8c263d1e77b6980a148602c87a80efb683ae
Wei Shi <wshi@redhat.com>
2024-02-05 10:34:14 +0800

    ci: add tekton pipeline jobs
    Signed-off-by: Wei Shi <wshi@redhat.com>

--- tmt/tests/test.sh
+++ tmt/tests/test.sh
@@ -3,15 +3,36 @@
 cd ../../
 ARCH=$(uname -m)
 export ARCH
-COMPONENT_NAME=$(echo -n "${SNAPSHOT}" | base64 -d | jq -r .components[0].name)
-CONTAINER_IMAGE=$(echo -n "${SNAPSHOT}" | base64 -d | jq -r .components[0].containerImage)
-echo "${SNAPSHOT}"
-echo "${COMPONENT_NAME}"
-echo "${CONTAINER_IMAGE}"
 
-if [ "$TEST_CASE" = "os-replace" ]; then
-	./os-replace.sh
+function run_tests() {
+	if [ "$TEST_CASE" = "os-replace" ]; then
+		./os-replace.sh
+	else
+		echo "Error: Test case $TEST_CASE not found!"
+		exit 1
+	fi
+}
+
+if [[ $CI == "RHTAP" ]]; then
+	echo "The base64 encoded snapshot is: ${SNAPSHOT}"
+	echo "It contains the following container images:"
+	IMAGES=$(echo "${SNAPSHOT}" | base64 -d | jq -r '.components[].containerImage')
+
+	for IMAGE in $IMAGES; do
+		skopeo inspect docker://"$IMAGE" >skopeo_inspect.json
+		COMPOSE_ID=$(jq -r '.Labels."redhat.compose-id"' skopeo_inspect.json)
+		if [[ "${COMPOSE_ID}" == "RHEL-9.4"* ]]; then
+			TEST_OS=$(echo "${COMPOSE_ID//./-}" | grep -oP '^RHEL-[0-9]+\-[0-9]+' | tr '[:upper:]' '[:lower:]')
+		elif [[ "${COMPOSE_ID}" == "CentOS-Stream-9"* ]]; then
+			TEST_OS=$(echo "$COMPOSE_ID" | grep -oP '^CentOS-Stream-[0-9]+' | tr '[:upper:]' '[:lower:]')
+		else
+			echo "Error: Compose ID is invalid: ${COMPOSE_ID}"
+			exit 1
+		fi
+		export IMAGE_URL="${IMAGE}"
+		export TEST_OS
+		run_tests
+	done
 else
-	echo "Error: Test case $TEST_CASE not found!"
-	exit 1
+	run_tests
 fi
