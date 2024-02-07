20950ff20b91ebf2b93d2538a2d527a1c9234cee
Wei Shi <wshi@redhat.com>
2024-02-07 21:29:15 +0800

    ci: copy the snapshot image to the test repo
    Signed-off-by: Wei Shi <wshi@redhat.com>

--- tmt/tests/test.sh
+++ tmt/tests/test.sh
@@ -14,25 +14,18 @@ function run_tests() {
 }
 
 if [[ $CI == "RHTAP" ]]; then
-	echo "The base64 encoded snapshot is: ${SNAPSHOT}"
-	echo "It contains the following container images:"
-	IMAGES=$(echo "${SNAPSHOT}" | base64 -d | jq -r '.components[].containerImage')
-
-	for IMAGE in $IMAGES; do
-		skopeo inspect docker://"$IMAGE" >skopeo_inspect.json
-		COMPOSE_ID=$(jq -r '.Labels."redhat.compose-id"' skopeo_inspect.json)
-		if [[ "${COMPOSE_ID}" == "RHEL-9.4"* ]]; then
-			TEST_OS=$(echo "${COMPOSE_ID//./-}" | grep -oP '^RHEL-[0-9]+\-[0-9]+' | tr '[:upper:]' '[:lower:]')
-		elif [[ "${COMPOSE_ID}" == "CentOS-Stream-9"* ]]; then
-			TEST_OS=$(echo "$COMPOSE_ID" | grep -oP '^CentOS-Stream-[0-9]+' | tr '[:upper:]' '[:lower:]')
-		else
-			echo "Error: Compose ID is invalid: ${COMPOSE_ID}"
-			exit 1
-		fi
-		export IMAGE_URL="${IMAGE}"
-		export TEST_OS
-		run_tests
-	done
+	skopeo inspect docker://"$IMAGE_URL" >skopeo_inspect.json
+	COMPOSE_ID=$(jq -r '.Labels."redhat.compose-id"' skopeo_inspect.json)
+	if [[ "${COMPOSE_ID}" == "RHEL-9.4"* ]]; then
+		TEST_OS=$(echo "${COMPOSE_ID//./-}" | grep -oP '^RHEL-[0-9]+\-[0-9]+' | tr '[:upper:]' '[:lower:]')
+	elif [[ "${COMPOSE_ID}" == "CentOS-Stream-9"* ]]; then
+		TEST_OS=$(echo "$COMPOSE_ID" | grep -oP '^CentOS-Stream-[0-9]+' | tr '[:upper:]' '[:lower:]')
+	else
+		echo "Error: Compose ID is invalid: ${COMPOSE_ID}"
+		exit 1
+	fi
+	export TEST_OS
+	run_tests
 else
 	run_tests
 fi
