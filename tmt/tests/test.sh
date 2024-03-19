5cd72e9ba88f1c57ada559764106088585e2c7ad
Xiaofeng Wang <henrywangxf@me.com>
2024-03-19 16:42:26 +0800

    test: add beaker bare metal os replace test
    Signed-off-by: Xiaofeng Wang <henrywangxf@me.com>

--- tmt/tests/test.sh
+++ tmt/tests/test.sh
@@ -6,6 +6,9 @@ TEMPDIR=$(mktemp -d)
 trap 'rm -rf -- "$TEMPDIR"' EXIT
 [[ $- =~ x ]] && debug=1 && set +x
 [[ -n "${GCP_SERVICE_ACCOUNT_FILE_B64+x}" ]] && echo "$GCP_SERVICE_ACCOUNT_FILE_B64" | base64 -d > "${TEMPDIR}"/gcp_auth.json && export GCP_SERVICE_ACCOUNT_FILE=${TEMPDIR}/gcp_auth.json
+[[ -n "${BEAKER_KEYTAB_B64+x}" ]] && echo "$BEAKER_KEYTAB_B64" | base64 -d > "${TEMPDIR}/beaker.keytab" && sudo mv "${TEMPDIR}/beaker.keytab" /etc/beaker.keytab
+[[ -n "${BEAKER_CLIENT_B64+x}" ]] && echo "$BEAKER_CLIENT_B64" | base64 -d > "${TEMPDIR}/client.conf" && sudo mv "${TEMPDIR}/client.conf" /etc/beaker/client.conf
+[[ -n "${KRB5_CONF_B64+x}" ]] && echo "$KRB5_CONF_B64" | base64 -d > "${TEMPDIR}/krb5.conf" && sudo mv "${TEMPDIR}/krb5.conf" /etc/krb5.conf
 [[ $debug == 1 ]] && set -x
 
 function run_tests() {
