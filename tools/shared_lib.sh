ea580cf62df5b81d4c7ea10f7153c69f0d977fcf
Xiaofeng Wang <henrywangxf@me.com>
2024-03-22 10:43:21 +0800

    test: fix shared_lib variables conflict with environment variables
    Signed-off-by: Xiaofeng Wang <henrywangxf@me.com>

--- tools/shared_lib.sh
+++ tools/shared_lib.sh
@@ -2,26 +2,26 @@
 
 # Dumps details about the instance running the CI job.
 function dump_runner {
-    CPUS=$(nproc)
-    MEM=$(free -m | grep -oP '\d+' | head -n 1)
-    DISK=$(df --output=size -h / | sed '1d;s/[^0-9]//g')
-    HOSTNAME=$(uname -n)
-    USER=$(whoami)
-    ARCH=$(uname -m)
-    KERNEL=$(uname -r)
+    RUNNER_CPUS=$(nproc)
+    RUNNER_MEM=$(free -m | grep -oP '\d+' | head -n 1)
+    RUNNER_DISK=$(df --output=size -h / | sed '1d;s/[^0-9]//g')
+    RUNNER_HOSTNAME=$(uname -n)
+    RUNNER_USER=$(whoami)
+    RUNNER_ARCH=$(uname -m)
+    RUNNER_KERNEL=$(uname -r)
 
     echo -e "\033[0;36m"
     cat << EOF
 ------------------------------------------------------------------------------
 CI MACHINE SPECS
 ------------------------------------------------------------------------------
-    Hostname: ${HOSTNAME}
-        User: ${USER}
-        CPUs: ${CPUS}
-         RAM: ${MEM} MB
-        DISK: ${DISK} GB
-        ARCH: ${ARCH}
-      KERNEL: ${KERNEL}
+    Hostname: ${RUNNER_HOSTNAME}
+        User: ${RUNNER_USER}
+        CPUs: ${RUNNER_CPUS}
+         RAM: ${RUNNER_MEM} MB
+        DISK: ${RUNNER_DISK} GB
+        ARCH: ${RUNNER_ARCH}
+      KERNEL: ${RUNNER_KERNEL}
 ------------------------------------------------------------------------------
 EOF
 }
